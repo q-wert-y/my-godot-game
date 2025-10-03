e79dac59fe2d985be23c39c79aea609ba2f683fe
q-wert-y <c5wnz@outlook.com>
2025-10-03 21:50:46 +0800

    lesson 4

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,6 +1,8 @@
 extends CharacterBody2D
 
 @export var move_speed : float = 200
+@export var animator : AnimatedSprite2D
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,7 +10,12 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	print(Input.get_vector("left","right","up","down"))
 	velocity = Input.get_vector("left","right","up","down") * move_speed
+	if velocity == Vector2.ZERO:
+		animator.play("idle")
+	else:
+		animator.play("run")
+	
 	move_and_slide()
