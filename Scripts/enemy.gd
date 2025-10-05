07381a07f99039bd0d07204e9296a33527bfcad5
q-wert-y <c5wnz@outlook.com>
2025-10-05 20:54:38 +0800

    lesson 7

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -2,12 +2,27 @@ extends Area2D
 
 @export var slime_speed : float = -50
 
+var is_dead : bool = false
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	position += Vector2(slime_speed,0) * delta
+	if not is_dead:
+		position += Vector2(slime_speed,0) * delta
 
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is CharacterBody2D:
 		print("Hit Player")
 		body.game_over()
+
+
+func _on_area_entered(area: Area2D) -> void:
+	if area.is_in_group("bullet"):
+		print("hit by bullet")
+		area.queue_free()
+		$AnimatedSprite2D.play("death")
+		is_dead = true
+		
+		await  get_tree().create_timer(0.6).timeout
+		queue_free()
+		
